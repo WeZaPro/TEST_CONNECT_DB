745c5f6075a56824c8f3f1208f550b23a573a9af
macbook <macbook@macbooks-MacBook-Pro.local>
2025-08-07 11:25:57 +0700

    update7

--- models/index.js
+++ models/index.js
@@ -10,10 +10,20 @@ const Sequelize = require("sequelize");
 //   }
 // );
 
-const sequelize = new Sequelize("happy_Demo", "happy_Demo", "Taweesak5050", {
-  host: "localhost",
-  dialect: "mysql",
-});
+// const sequelize = new Sequelize("happy_Demo", "happy_Demo", "Taweesak5050", {
+//   host: "localhost",
+//   dialect: "mysql",
+// });
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  "happy_Demo",
+  "Taweesak5050",
+  {
+    host: "localhost",
+    dialect: "mysql",
+  }
+);
 
 const db = {};
 db.Sequelize = Sequelize;
